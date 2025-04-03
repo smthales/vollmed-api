1e11c67f5fe1b54eb5fe79706939445cb8ea867e
Thales de Souza Machado <thalesabranchess@gmail.com>
2025-04-02 23:00:41 -0300

    implementando as rotas de PUT e DELETE da API

---
+++ src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
@@ -0,0 +1,12 @@
+package med.voll.api.medico;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosAtualizacaoMedico(@NotNull Long id,
+                                     String nome,
+                                     String telefone,
+                                     DadosEndereco endereco) {
+}
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -26,16 +26,22 @@ public class Medico {
     private Especialidade especialidade;
     @Embedded
     private Endereco endereco;
+    private Boolean ativo;
 
     public Medico() {}
 
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
     }
 
     public Long getId() {
@@ -92,5 +98,23 @@ public class Medico {
 
     public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+
+        if(dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+
+        if(dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
